c37d3ec8ca76ea41f4d2eb39febaab5d2646791f
Olivier <o.bonsignour@castsoftware.com>
2024-03-04 19:09:53 -0500

    Changes the LIMIT

--- sam-graf-server.py
+++ sam-graf-server.py
@@ -32,7 +32,7 @@ def get_objects(app_name):
         query = query + " LIMIT $limit"
         return my_query.execute_query(query, limit)
     else:
-        query = query + " LIMIT 250"
+        query = query + " LIMIT 50"
         return my_query.execute_query(query)
 
 
