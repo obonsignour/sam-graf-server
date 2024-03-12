c722b600e2b0799c25e9903d47652b842313df0a
Olivier <o.bonsignour@castsoftware.com>
2024-03-11 22:05:54 -0400

    Replaced graph_name by elementId as there are several graphs with the same name

--- sam-graf-server.py
+++ sam-graf-server.py
@@ -31,17 +31,17 @@ def get_applications():
 
 @app.route('/Applications/<app_name>/DataGraphs', methods=['GET'])
 def get_datagraphs(app_name):
-    logging.info("Getting applications")
+    logging.info("Getting Datagraphs")
     my_query = NeoQuery(URI, AUTH, DATABASE)
-    query = f"MATCH (a:DataGraph:{app_name}) RETURN a.Name AS graphName ORDER BY graphName"
+    query = f"MATCH (a:DataGraph:{app_name}) RETURN elementId(a) AS id, a.Name AS name ORDER BY name"
     return my_query.execute_query(query)
 
 
 @app.route('/Applications/<app_name>/Transactions', methods=['GET'])
 def get_transactions(app_name):
-    logging.info("Getting applications")
+    logging.info("Getting Transactions")
     my_query = NeoQuery(URI, AUTH, DATABASE)
-    query = f"MATCH (a:Transaction:{app_name}) RETURN a.Name AS graphName ORDER BY graphName"
+    query = f"MATCH (a:Transaction:{app_name}) RETURN elementId(a) AS id, a.Name AS name ORDER BY name"
     return my_query.execute_query(query)
 
 
@@ -60,14 +60,15 @@ def get_objects(app_name):
         return my_query.execute_query(query)
 
 
-def __graphs_query(app_name: str, graph_type: str, relationship_type: str, graph_name: str) -> str:
+def __graphs_query(app_name: str, graph_type: str, relationship_type: str, element_id: str) -> str:
     return (
         f"CALL cast.linkTypes([\"CALL_IN_TRAN\"]) yield linkTypes\n"
         f"WITH linkTypes + [\"STARTS_WITH\", \"ENDS_WITH\"] AS updatedLinkTypes\n"
         f"MATCH (d:{graph_type}:{app_name})<-[:{relationship_type}]-(n)\n"
         f"WITH collect(id(n)) AS nodeIds,updatedLinkTypes\n"
-        f"MATCH p=(d:{graph_type}:{app_name} {{Name: '{graph_name}'}})<-[:{relationship_type}]-(n:{app_name})<-[r]-(m:{app_name})-[:{relationship_type}]->(d)\n"
-        f"WHERE (n:Object OR n:SubObject)\n"
+        f"MATCH p=(d:{graph_type}:{app_name})<-[:{relationship_type}]-(n:{app_name})<-[r]-(m:{app_name})-[:{relationship_type}]->(d)\n"
+        f"WHERE elementId(d) = '{element_id}'\n"
+        f"AND (n:Object OR n:SubObject)\n"
         f"AND (m:Object OR m:SubObject)\n"
         f"AND id(n) IN nodeIds AND id(m) IN nodeIds\n"
         f"AND type(r) IN updatedLinkTypes\n"
@@ -75,17 +76,17 @@ def __graphs_query(app_name: str, graph_type: str, relationship_type: str, graph
     )
 
 
-@app.route('/Applications/<app_name>/Transactions/<graph_name>', methods=['GET'])
-def get_transaction(app_name, graph_name):
+@app.route('/Applications/<app_name>/Transactions/<element_id>', methods=['GET'])
+def get_transaction(app_name, element_id):
     my_query = NeoQuery(URI, AUTH, DATABASE)
-    cypher_query = __graphs_query(app_name, "Transaction", "IS_IN_TRANSACTION", graph_name)
+    cypher_query = __graphs_query(app_name, "Transaction", "IS_IN_TRANSACTION", element_id)
     return my_query.execute_query(cypher_query)
 
 
-@app.route('/Applications/<app_name>/DataGraphs/<graph_name>', methods=['GET'])
-def get_datagraph(app_name, graph_name):
+@app.route('/Applications/<app_name>/DataGraphs/<element_id>', methods=['GET'])
+def get_datagraph(app_name, element_id):
     my_query = NeoQuery(URI, AUTH, DATABASE)
-    cypher_query = __graphs_query(app_name, "DataGraph", "IS_IN_DATAGRAPH", graph_name)
+    cypher_query = __graphs_query(app_name, "DataGraph", "IS_IN_DATAGRAPH", element_id)
     return my_query.execute_query(cypher_query)
 
 
